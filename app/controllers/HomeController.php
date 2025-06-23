ab03bb688b1b9c370b6a46d9962e87acb62a1d7f
Daniel <dante@teste.com>
2025-06-23 20:53:03 -0300

    Criado a pagina home com uma estrutura contendo sidebar com as funcionalidades pretendidas atualmente.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -8,14 +8,26 @@ class HomeController
     public function index()
     {
 
+        $loader = new \Twig\Loader\FilesystemLoader([
+            '../app/views',
+            '../app/templates'
+        ]);
+        $twig = new \Twig\Environment($loader, [
+            'cache' => false, // Desative no desenvolvimento
+        ]);
+
+        $template = $twig->load('home.html');
+
+        $params = []; // Aqui você pode passar variáveis para o template
+
+        echo $template->render($params);
+
         session_start();
 
         if (!isset($_SESSION['id'])) {
             header('Location: /login');
             exit;
         }
-
-        echo "Bem-vindo, " . $_SESSION['username'];
 
     }
 }
